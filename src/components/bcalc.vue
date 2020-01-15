f29391a73e9eae22e7d093b3061c4ebb24e1f3e7
Mindas <mindaugas.lickus@webelevate.ie>
2020-01-15 21:13:07 +0000

    adding component with calc

--- src/components/bcalc.vue
+++ src/components/bcalc.vue
@@ -1,10 +1,40 @@
 <template>
-  <div></div>
+  <div>
+    <p>{{name}} - {{daysUntilBirthday(bdate)}} days, age: {{years}} years, total lived {{liveddays}} days. {{bdate}} </p>
+  </div>
 </template>
 
 <script>
+import moment from "moment";
+
 export default {
-  props: {}
+  props: {
+    bdate: String,
+    name: String
+  },
+  data() {
+    return {
+      today: moment().format("YYYY-MM-DD")
+    };
+  },
+  computed: {
+    years: function() {
+      return moment().diff(this.bdate, "years");
+    },
+    liveddays: function() {
+      return moment().diff(this.bdate, "days");
+    }
+  },
+  methods: {
+    daysUntilBirthday: function(val) {
+      var birthday = moment(val).format("YYYY-MM-DD");
+      var years = moment().diff(birthday, "years");
+      var adjustToday =
+        birthday.substring(5) === this.today.substring(5) ? 0 : 1;
+      var nextBirthday = moment(birthday).add(years + adjustToday, "years");
+      return nextBirthday.diff(this.today, "days");
+    }
+  }
 };
 </script>
 
--- src/components/birthday.vue
+++ src/components/birthday.vue
@@ -3,30 +3,36 @@
     <div class="container">
       <h1>Days until birthday</h1>
       <div class="list">
-        Kotryna -
-        <span style="color:red;">{{ daysUntilBirthday1("1999-04-15") }}</span>
-        days
+        Kotryna - {{ daysUntilBirthday("1999-04-15") }} days
         <br />
-        Mindaugas - {{ daysUntilBirthday1("1979-04-21") }} days <br />
-        Arminas - {{ daysUntilBirthday1("1979-04-22") }} days <br />
-        Algis - {{ daysUntilBirthday1("1944-04-26") }} days <br />
-        Zita - {{ daysUntilBirthday1("1982-05-09") }} days <br />
-        Uma - {{ daysUntilBirthday1("2012-07-09") }} days <br />
-        Laimute - {{ daysUntilBirthday1("1958-07-18") }} days <br />
-        Ilajus - {{ daysUntilBirthday1("2014-12-17") }} days <br />
+        Mindaugas - {{ daysUntilBirthday("1979-04-21") }} days <br />
+        Arminas - {{ daysUntilBirthday("1979-04-22") }} days <br />
+        Algis - {{ daysUntilBirthday("1944-04-26") }} days <br />
+        Zita - {{ daysUntilBirthday("1982-05-09") }} days <br />
+        Uma - {{ daysUntilBirthday("2012-07-09") }} days <br />
+        Laimute - {{ daysUntilBirthday("1958-07-18") }} days <br />
+        Ilajus - {{ daysUntilBirthday("2014-12-17") }} days <br />
       </div>
+      <bcalc bdate="2008-04-22" name="Arminas" />
+      <bcalc bdate="1944-04-26" name="Algis" />
+      <bcalc bdate="2014-12-17" name="Ilajus" />
+      <bcalc bdate="2000-01-01" name="JOE" />
     </div>
   </div>
 </template>
 
 <script>
 import moment from "moment";
+import bcalc from "@/components/bcalc.vue";
 
 export default {
   data() {
     return {
       today: moment().format("YYYY-MM-DD")
     };
+  },
+  components: {
+    bcalc
   },
   methods: {
     //Calculate current age of person in years (moment truncates by default)
@@ -36,15 +42,7 @@ export default {
     // liveddays: function() {
     //   return moment().diff(this.birthday, "days");
     // },
-    daysUntilBirthday: function() {
-      var birthday = moment().format("YYYY-MM-DD");
-      var years = moment().diff(birthday, "years");
-      var today = moment().format("YYYY-MM-DD");
-      var adjustToday = birthday.substring(5) === today.substring(5) ? 0 : 1;
-      var nextBirthday = moment(birthday).add(years + adjustToday, "years");
-      return nextBirthday.diff(today, "days");
-    },
-    daysUntilBirthday1: function(val) {
+    daysUntilBirthday: function(val) {
       var birthday = moment(val).format("YYYY-MM-DD");
       var years = moment().diff(birthday, "years");
       var adjustToday =
