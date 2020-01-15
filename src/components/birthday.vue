7844e4fdca3db092c22dac6467a86c93e91da907
Mindas <mindaugas.lickus@webelevate.ie>
2020-01-15 11:45:15 +0000

    working if date hardcoded in template

--- src/components/birthday.vue
+++ src/components/birthday.vue
@@ -2,18 +2,17 @@
   <div>
     <h1>Days until birthday</h1>
     <br />
-    <div v-for="person in persons" :key="person.name">
-      -{{ person.name }} -> {{ person.birthdayb }} -
-    </div>
+
+    Days until Birthday: <br /><br />
+    <!-- <span style="color:red;">{{ daysUntilBirthday }}</span> days.. -->
+    Kotryna -
+    <span style="color:red;">{{ daysUntilBirthday1("1999-04-15") }}</span> days
     <br />
-    <!-- {{ years }} -->
-
-    Days until Birthday:
-    <span style="color:red;">{{ daysUntilBirthday }}</span> days..
-
-    <br />
-
-    Total lived <span class="days">{{ liveddays }}</span> days.
+    Mindaugas - {{ daysUntilBirthday1("1979-04-21") }} days <br />
+    Arminas - {{ daysUntilBirthday1("1979-04-22") }} days <br />
+    Zita - {{ daysUntilBirthday1("1982-05-09") }} days <br />
+    Uma - {{ daysUntilBirthday1("2012-07-09") }} days <br />
+    Ilajus - {{ daysUntilBirthday1("2014-12-17") }} days <br />
     <br />
   </div>
 </template>
@@ -38,31 +37,63 @@ export default {
           name: "Eve",
           birthdayb: "2012-07-09"
         }
-      ]
+      ],
+      today: moment().format("YYYY-MM-DD")
       //   birthday: moment("2014-11-14").format("YYYY-MM-DD")
     };
   },
-  computed: {
-    birthday: function() {
-      //return  moment(this.person.birthdayb).format('YYYY-MM-DD')
-      return moment(this.person.birthday).format("YYYY-MM-DD");
-    },
+  //   computed: {
+  //     birthday: function() {
+  //       //return  moment(this.person.birthdayb).format('YYYY-MM-DD')
+  //       return moment(this.person.birthday).format("YYYY-MM-DD");
+  //     },
+  //     //Calculate current age of person in years (moment truncates by default)
+  //     years: function() {
+  //       return moment().diff(this.birthday, "years");
+  //     },
+  //     liveddays: function() {
+  //       return moment().diff(this.birthday, "days");
+  //     },
+  //     daysUntilBirthday: function() {
+  //       let today = moment().format("YYYY-MM-DD");
+  //       let adjustToday =
+  //         this.birthday.substring(5) === today.substring(5) ? 0 : 1;
+  //       let nextBirthday = moment(this.birthday).add(
+  //         this.years + adjustToday,
+  //         "years"
+  //       );
+  //       return nextBirthday.diff(this.today, "days");
+  //     }
+  //   },
+  methods: {
+    // birthday: function() {
+    //return  moment(this.person.birthdayb).format('YYYY-MM-DD')
+    //   return moment(this.person.birthday).format("YYYY-MM-DD");
+    // },
     //Calculate current age of person in years (moment truncates by default)
-    years: function() {
-      return moment().diff(this.birthday, "years");
-    },
-    liveddays: function() {
-      return moment().diff(this.birthday, "days");
-    },
+    // years: function() {
+    //   return moment().diff(this.birthday, "years");
+    // },
+    // liveddays: function() {
+    //   return moment().diff(this.birthday, "days");
+    // },
     daysUntilBirthday: function() {
-      let today = moment().format("YYYY-MM-DD");
-      let adjustToday =
-        this.birthday.substring(5) === today.substring(5) ? 0 : 1;
-      let nextBirthday = moment(this.birthday).add(
-        this.years + adjustToday,
-        "years"
-      );
+      var birthday = moment().format("YYYY-MM-DD");
+      var years = moment().diff(birthday, "years");
+      var today = moment().format("YYYY-MM-DD");
+      var adjustToday = birthday.substring(5) === today.substring(5) ? 0 : 1;
+      var nextBirthday = moment(birthday).add(years + adjustToday, "years");
+      return nextBirthday.diff(today, "days");
+    },
+    daysUntilBirthday1: function(val) {
+      var birthday = moment(val).format("YYYY-MM-DD");
+      var years = moment().diff(birthday, "years");
+      //   var today = moment().format("YYYY-MM-DD");
+      var adjustToday =
+        birthday.substring(5) === this.today.substring(5) ? 0 : 1;
+      var nextBirthday = moment(birthday).add(years + adjustToday, "years");
       return nextBirthday.diff(this.today, "days");
+      //   return adjustToday;
     }
   }
 };
